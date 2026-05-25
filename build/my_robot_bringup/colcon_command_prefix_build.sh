4de866aad2e9b903fa75f1ead3a27e084745b739
lucasSCsantos <luc.cristovam10@gmail.com>
2026-05-24 23:10:21 -0300

    graca

---
+++ build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/lucas/Projetos/faculdade/roz_prova_1/install/my_robot_description/share/my_robot_description/package.sh"
--- install/setup.ps1
+++ install/setup.ps1
@@ -24,6 +24,7 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/jazzy/local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/lucas/Projetos/faculdade/roz_prova_1/src/install/local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
